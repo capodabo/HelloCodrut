57562d1f1eafa95558aadbf2550210e10805a148
capodabo <71343016+capodabo@users.noreply.github.com>
2020-09-18 19:26:52 +0100

    Task no.2

--- src/MyTimerTask.java
+++ src/MyTimerTask.java
@@ -1,33 +1,57 @@
-import java.time.LocalTime;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MyTimerTask extends TimerTask {
+public class MyTimerTask {
 
-    public static void timeChecker() {
-        LocalTime time = LocalTime.now();
-        LocalTime calculation = time.minusMinutes(150);
+    public static void httpRead() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://worldtimeapi.org/api/timezone/Europe/Bucharest.txt"))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String worldTime[] = response.body().split("\n");
+        String utcDateTime1 = worldTime[12];
+        String utcDateTime2[] = utcDateTime1.split(": ");
+        String utcDateTime3 = utcDateTime2[1];
+        ZonedDateTime localTime = ZonedDateTime.parse(utcDateTime3);
+        ZonedDateTime timeCalculation = localTime.minusMinutes(150);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        int minute = time.getMinute();
+        int minute = localTime.getMinute();
 
         if (minute % 2 == 1) {
-            System.out.println("The time is now " + time.format(formatter));
-            System.out.println("Codrut has asked me to print t - 2h30m which is " + calculation.format(formatter));
-            System.out.println("");
+            System.out.println("The time is now " + localTime.format(formatter));
+            System.out.println("Codrut has asked me to print t - 2h30m which is " + timeCalculation.format(formatter) + "\n");
+            System.out.println("WorldTime API text:" + "\n" + response.body() + "\n");
         }
     }
 
-    public void run() {
-    }
-
-    public static void main(String[] args) {
+    public static void timer() {
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
-                timeChecker();
+                try {
+                    httpRead();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }, 0, 60 * 1000);
     }
+    public static void main(String[] args) {
+        timer();
+    }
 }
-// Task completed
+
+// Task completed - Strutocamila achieved!
+
